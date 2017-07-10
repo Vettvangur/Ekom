c13670ef3598c98baaa2ec6b06e7d65b8d4d97e7
Garðar Þorsteinsson <gardar@vettvangur.is>
2017-07-10 13:42:48 +0000

    stock repo

---
+++ uWebshop/Models/Data/StockData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Umbraco.Core.Persistence;
+using uWebshop.Interfaces;
+
+namespace uWebshop.Models.Data
+{
+    [TableName("uWebshopStock")]
+    [PrimaryKey("UniqueId", autoIncrement = false)]
+    public class StockData
+    {
+        public Guid UniqueId { get; set; }
+        public int Stock { get; set; }
+        public Guid NodeId { get; set; }
+        public string StoreAlias { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime UpdateDate { get; set; }
+    }
+}
---
+++ uWebshop/Repository/StockRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Umbraco.Core;
+using uWebshop.Models.Data;
+
+namespace uWebshop.Repository
+{
+    public class StockRepository
+    {
+        public StockData GetStockById(Guid uniqueId)
+        {
+            using (var db = ApplicationContext.Current.DatabaseContext.Database)
+            {
+                return db.FirstOrDefault<StockData>("SELECT * FROM uWebshopStock WHERE UniqueId = @0", uniqueId);
+            }
+        }
+        public IEnumerable<StockData> GetStockByNodeId(Guid uniqueId)
+        {
+            using (var db = ApplicationContext.Current.DatabaseContext.Database)
+            {
+                return db.Query<StockData>("SELECT * FROM uWebshopStock WHERE NodeId = @0", uniqueId);
+            }
+        }
+
+        public IEnumerable<StockData> GetStockByStore(string storeAlias)
+        {
+            using (var db = ApplicationContext.Current.DatabaseContext.Database)
+            {
+                return db.Query<StockData>("SELECT * FROM uWebshopStock WHERE StoreAlias = @0", storeAlias);
+            }
+        }
+
+        public IEnumerable<StockData> GetStockByNodeIdAndStore(Guid uniqueId, string storeAlias)
+        {
+            using (var db = ApplicationContext.Current.DatabaseContext.Database)
+            {
+                return db.Query<StockData>("SELECT * FROM uWebshopStock WHERE StoreAlias = @0 AND NodeId = @1", storeAlias, uniqueId);
+            }
+        }
+    }
+}
