9ade280d021bdbb22aee6c0ee4c8183f51a8024f
Gunnar Már Óttarsson <gunni@starcraft.is>
2017-10-19 11:46:35 +0000

    GMO.Vorto work

--- uWebshop.Site/App_Plugins/Vorto/js/vorto.js
+++ uWebshop.Site/App_Plugins/Vorto/js/vorto.js
@@ -42,9 +42,9 @@
             config: {},
             view: ""
         };
-        
+
         if (!angular.isObject($scope.model.value))
-            $scope.model.value = {};
+            $scope.model.value = undefined;
 
         $scope.model.value = $scope.model.value || {
             values: {},
@@ -395,11 +395,13 @@ angular.module("umbraco.directives").directive('vortoProperty',
             scope.model.config = angular.copy(scope.config);
 
             scope.model.alias = scope.propertyAlias + "." + scope.language;
-            scope.model.value = scope.value.values[scope.language];
+            scope.model.value = scope.value.values ? scope.value.values[scope.language] : undefined;
 
             var unsubscribe = scope.$on("vortoSyncLanguageValue", function (ev, args) {
                 if (args.language === scope.language) {
                     scope.$broadcast("formSubmitting", { scope: scope });
+                    if (!scope.value.values)
+                        scope.value.values = {};
                     scope.value.values[scope.language] = scope.model.value;
                 }
             });
