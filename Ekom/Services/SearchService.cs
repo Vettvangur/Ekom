83f0061e50aaff14d21141cdb80a41aaa428f34d
Garðar Þorsteinsson <gardar@vettvangur.is>
2017-12-12 12:44:49 +0000

    search service

---
+++ Ekom/Services/SearchService.cs
@@ -0,0 +1,88 @@
+﻿using Examine;
+using log4net;
+using Lucene.Net.QueryParsers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ekom.Services
+{
+    public class SearchService
+    {
+        /// <summary>
+        /// Builds and run the Lucene query.
+        /// </summary>
+        public static ISearchResults SearchResult(string query, string searchProvider, out int total)
+        {
+            total = 0;
+
+            try
+            {
+                var searcher = ExamineManager.Instance.SearchProviderCollection[searchProvider];
+                var luceneQuery = new StringBuilder();
+
+                if (!string.IsNullOrWhiteSpace(query))
+                {
+
+                    var searchTerms = query
+                        .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                        .Select(QueryParser.Escape)
+                        .SelectMany(st => new[] { st + "* " + st + "~0.6" });
+
+                    int i = 0;
+                    foreach (var term in searchTerms)
+                    {
+                        if (i != 0)
+                        {
+                            luceneQuery.Append(" AND ");
+                        }
+
+                        if (i == 0)
+                        {
+                            luceneQuery.Append("+");
+                        }
+
+                        luceneQuery.Append("(");
+                        luceneQuery.Append(term);
+                        luceneQuery.Append(")");
+
+                        i++;
+                    }
+                }
+
+                ISearchResults searchResults;
+
+                // If no filters were selected search for nodes with ID 0 that will return no results.
+                if (luceneQuery.Length > 0)
+                {
+                    var rawQuery = searcher.CreateSearchCriteria().RawQuery(luceneQuery.ToString());
+                    searchResults = searcher.Search(rawQuery);
+                }
+                else
+                {
+                    searchResults = searcher.Search(searcher.CreateSearchCriteria().Id(0).Compile());
+                }
+
+                total = searchResults.TotalItemCount;
+
+                return searchResults;
+
+            }
+            catch (Exception ex)
+            {
+                Log.Error("SearchResult Error!", ex);
+
+                return null;
+            }
+
+        }
+
+        private static readonly ILog Log =
+            LogManager.GetLogger(
+                MethodBase.GetCurrentMethod().DeclaringType
+            );
+    }
+}
--- Ekom/Utilities/DictionaryExtensions.cs
+++ Ekom/Utilities/DictionaryExtensions.cs
@@ -23,6 +23,20 @@ namespace Ekom.Utilities
         }
 
         /// <summary>
+        /// Get value in properties
+        /// </summary>
+        /// <param name="alias"></param>
+        public static string GetProperty(this IDictionary<string, string> properties, string alias)
+        {
+            if (properties.Any(x => x.Key == alias))
+            {
+                return properties.FirstOrDefault(x => x.Key == alias).Value;
+            }
+
+            return string.Empty;
+        }
+
+        /// <summary>
         /// Get value in properties by store
         /// </summary>
         /// <param name="alias"></param>
