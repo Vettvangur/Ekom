f5c98bd789954ea67220e9fca9d9f0b4a9c9faaa
Garðar Þorsteinsson <gardar@vettvangur.is>
2018-04-27 15:25:23 +0000

    routing for ekomManager

--- Ekom.Manager/src/app.js
+++ Ekom.Manager/src/app.js
@@ -1,9 +1,21 @@
-import 'styles/app.scss';
+﻿import 'styles/app.scss';
 
-function test() {
+// Expose React and ReactDOM globally, used by ReactJS.NET SSR.
+import 'expose-loader?React!react';
+import 'expose-loader?ReactDOM!react-dom';
 
-    alert('hallo');
-}
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-test();
 
+const AppManager = props => (
+    <Router>
+        <h1>asdasd</h1>
+    </Router>
+);
+
+ReactDOM.render(AppManager, document.getElementById('app'));
+
+// We could also wrap this module with a new base file and require+expose this module from there
+// Instead we shortcut and access webpack's internal way of globalizing modules
+global['AppManager'] = AppManager;
---
+++ Ekom/App_Start/IAppEventHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Web.Routing;
+using umbraco;
+using Umbraco.Core;
+
+namespace Ekom.App_Start
+{
+    class IAppEventHandler : IApplicationEventHandler
+    {
+        /// <summary> 
+        /// Umbraco lifecycle method 
+        /// </summary>  
+        /// <param name="httpApplication"></param> 
+        /// <param name="applicationContext"></param> 
+        public void OnApplicationStarted(UmbracoApplicationBase httpApplication, ApplicationContext applicationContext)
+        {
+            RouteTable.Routes.MapRoute(
+                name: "ekomManager",
+                url: GlobalSettings.UmbracoMvcArea + "/backoffice/ekomManager/{action}",
+                defaults: new
+                {
+                    controller = "EkomManager",
+                    action = "Index",
+                    id = UrlParameter.Optional
+                });
+        }
+
+        public void OnApplicationInitialized(UmbracoApplicationBase httpApplication, ApplicationContext applicationContext)
+        {
+        }
+
+        public void OnApplicationStarting(UmbracoApplicationBase httpApplication, ApplicationContext applicationContext)
+        {
+        }
+    }
+}
