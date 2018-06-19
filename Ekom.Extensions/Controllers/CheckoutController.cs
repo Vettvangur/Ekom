96096cbc5e383a6d3e4ed0a8e67d722857125fa4
Garðar <gardar@vettvangur.is>
2018-06-19 18:51:02 +0000

    update checkout pay, redirect error

--- Ekom.Extensions/Controllers/CheckoutController.cs
+++ Ekom.Extensions/Controllers/CheckoutController.cs
@@ -46,157 +46,166 @@ namespace Ekom.Extensions.Controllers
         /// <returns></returns>
         public async Task<ActionResult> Pay(PaymentRequest paymentRequest)
         {
-            var hangfireJobs = new List<string>();
-
-            var order = Order.Instance.GetOrder();
-            var ekomPP = Providers.Instance.GetPaymentProvider(paymentRequest.PaymentProvider);
-
-            var pp = NetPayment.Current.GetPaymentProvider(ekomPP.Name);
-
-            if (_config.StoreCustomerData)
+            try
             {
-                using (var db = DatabaseContext.Database)
+                var hangfireJobs = new List<string>();
+
+                var order = Order.Instance.GetOrder();
+                var ekomPP = Providers.Instance.GetPaymentProvider(paymentRequest.PaymentProvider);
+
+                var pp = NetPayment.Current.GetPaymentProvider(ekomPP.Name);
+
+                if (_config.StoreCustomerData)
                 {
-                    db.Insert(new CustomerData
+                    using (var db = DatabaseContext.Database)
                     {
-                        // Unfinished
-                    });
+                        db.Insert(new CustomerData
+                        {
+                            // Unfinished
+                        });
+                    }
                 }
-            }
 
-            var orderItems = new List<OrderItem>();
-            foreach (var line in order.OrderLines)
-            {
-                try
+                var orderItems = new List<OrderItem>();
+                foreach (var line in order.OrderLines)
                 {
-                    if (!line.Product.Backorder)
+                    try
                     {
-                        if (line.Product.VariantGroups.Any())
+                        if (!line.Product.Backorder)
                         {
-                            foreach (var variant in line.Product.VariantGroups.SelectMany(x => x.Variants))
+                            if (line.Product.VariantGroups.Any())
                             {
-                                var variantStock = Stock.Instance.GetStock(variant.Key);
-
-                                if (variantStock >= line.Quantity)
+                                foreach (var variant in line.Product.VariantGroups.SelectMany(x => x.Variants))
                                 {
-                                    hangfireJobs.Add(_stock.ReserveStock(variant.Key, (line.Quantity * -1)));
-                                }
-                                else
-                                {
-                                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest, "Not enough stock available");
-                                }
-                            }
+                                    var variantStock = Stock.Instance.GetStock(variant.Key);
 
-                        }
-                        else
-                        {
-                            var productStock = Stock.Instance.GetStock(line.ProductKey);
+                                    if (variantStock >= line.Quantity)
+                                    {
+                                        hangfireJobs.Add(_stock.ReserveStock(variant.Key, (line.Quantity * -1)));
+                                    }
+                                    else
+                                    {
+                                        return RedirectToCurrentUmbracoPage("?errorStatus=stockError");
+                                    }
+                                }
 
-                            if (productStock >= line.Quantity)
-                            {
-                                hangfireJobs.Add(_stock.ReserveStock(line.ProductKey, (line.Quantity * -1)));
                             }
                             else
                             {
-                                return new HttpStatusCodeResult(HttpStatusCode.BadRequest, "Not enough stock available");
+                                var productStock = Stock.Instance.GetStock(line.ProductKey);
+
+                                if (productStock >= line.Quantity)
+                                {
+                                    hangfireJobs.Add(_stock.ReserveStock(line.ProductKey, (line.Quantity * -1)));
+                                }
+                                else
+                                {
+                                    return RedirectToCurrentUmbracoPage("?errorStatus=stockError");
+                                }
+                            }
+                        }
+
+
+                        if (line.Discount != null)
+                        {
+                            hangfireJobs.Add(_stock.ReserveDiscountStock(line.Discount.Key, 1, line.Coupon));
+
+                            if (line.Discount.HasMasterStock)
+                            {
+                                hangfireJobs.Add(_stock.ReserveDiscountStock(line.Discount.Key, 1));
                             }
                         }
                     }
+                    catch (StockException)
+                    {
+                        return RedirectToCurrentUmbracoPage("?errorStatus=stockError");
+                    }
 
+                    orderItems.Add(new OrderItem
+                    {
+                        GrandTotal = line.Amount.Value,
+                        Price = line.Product.VariantGroups.Any() && line.Product.VariantGroups.Any(x => x.Variants.Any()) ? line.Product.VariantGroups.First().Variants.First().Price.BeforeDiscount.Value : line.Product.Price.BeforeDiscount.Value,
+                        Title = line.Product.Title,
+                        Quantity = line.Quantity,
+                    });
 
                     if (line.Discount != null)
                     {
-                        hangfireJobs.Add(_stock.ReserveDiscountStock(line.Discount.Key, 1, line.Coupon));
-
-                        if (line.Discount.HasMasterStock)
+                        orderItems.Add(new OrderItem
                         {
-                            hangfireJobs.Add(_stock.ReserveDiscountStock(line.Discount.Key, 1));
+                            Title = "Line discount " + line.Discount.Amount.Type,
+                            Price = -line.Discount.Amount.Amount,
+                            Quantity = 1,
+                            GrandTotal = -line.Discount.Amount.Amount,
+                        });
+                    }
+                }
+
+                if (order.Discount != null)
+                {
+                    try
+                    {
+                        hangfireJobs.Add(_stock.ReserveDiscountStock(order.Discount.Key, 1, order.Coupon));
+
+                        if (order.Discount.HasMasterStock)
+                        {
+                            hangfireJobs.Add(_stock.ReserveDiscountStock(order.Discount.Key, 1));
                         }
                     }
-                }
-                catch (StockException)
-                {
-                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest, "Not enough stock available");
-                }
-
-                orderItems.Add(new OrderItem
-                {
-                    GrandTotal = line.Amount.Value,
-                    Price = line.Product.Price.BeforeDiscount.Value,
-                    Title = line.Product.Title,
-                    Quantity = line.Quantity,
-                });
-
-                if (line.Discount != null)
-                {
-                    orderItems.Add(new OrderItem
+                    catch (StockException)
                     {
-                        Title = "Line discount " + line.Discount.Amount.Type,
-                        Price = -line.Discount.Amount.Amount,
-                        Quantity = 1,
-                        GrandTotal = -line.Discount.Amount.Amount,
-                    });
-                }
-            }
-
-            if (order.Discount != null)
-            {
-                try
-                {
-                    hangfireJobs.Add(_stock.ReserveDiscountStock(order.Discount.Key, 1, order.Coupon));
-
-                    if (order.Discount.HasMasterStock)
-                    {
-                        hangfireJobs.Add(_stock.ReserveDiscountStock(order.Discount.Key, 1));
+                        return new HttpStatusCodeResult(HttpStatusCode.BadRequest, "Not enough discount stock available");
                     }
                 }
-                catch (StockException)
+
+                if (paymentRequest.ShippingProvider != Guid.Empty)
                 {
-                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest, "Not enough discount stock available");
+                    var ekomSP = Providers.Instance.GetShippingProvider(paymentRequest.ShippingProvider);
+
+                    if (ekomSP.Price.Value > 0)
+                    {
+                        orderItems.Add(new OrderItem
+                        {
+                            GrandTotal = ekomSP.Price.Value,
+                            Price = ekomSP.Price.Value,
+                            Title = ekomSP.Title,
+                            Quantity = 1,
+                        });
+                    }
+
                 }
-            }
 
-            if (paymentRequest.ShippingProvider != Guid.Empty)
-            {
-                var ekomSP = Providers.Instance.GetShippingProvider(paymentRequest.ShippingProvider);
-
-                if (ekomSP.Price.Value > 0)
+                if (order.Discount != null)
                 {
                     orderItems.Add(new OrderItem
                     {
-                        GrandTotal = ekomSP.Price.Value,
-                        Price = ekomSP.Price.Value,
-                        Title = ekomSP.Title,
+                        Title = "Order discount " + order.Discount.Amount.Type,
                         Quantity = 1,
+                        Price = order.Discount.Amount.Amount,
+                        GrandTotal = order.Discount.Amount.Amount,
                     });
                 }
 
-            }
+                // save job ids to sql for retrieval after checkout completion
+                Order.Instance.AddHangfireJobsToOrder(hangfireJobs);
 
-            if (order.Discount != null)
+                return Content(await pp.RequestAsync(
+                    order.ChargedAmount.Value,
+                    orderItems,
+                    skipReceipt: true,
+                    vortoLanguage: order.StoreInfo.Alias,
+                    language: "IS", //TODO needs to come from Store, but we can not use culture
+                    member: Umbraco.MembershipHelper.GetCurrentMemberId(),
+                    orderCustomString: order.UniqueId.ToString(),
+                    paymentProviderId: paymentRequest.PaymentProvider.ToString()
+                ));
+            }
+            catch (Exception ex)
             {
-                orderItems.Add(new OrderItem
-                {
-                    Title = "Order discount " + order.Discount.Amount.Type,
-                    Quantity = 1,
-                    Price = order.Discount.Amount.Amount,
-                    GrandTotal = order.Discount.Amount.Amount,
-                });
+                _log.Error("Checkout payment failed!",ex);
+                return RedirectToCurrentUmbracoPage("?errorStatus=serverError");
             }
 
-            // save job ids to sql for retrieval after checkout completion
-            Order.Instance.AddHangfireJobsToOrder(hangfireJobs);
-
-            return Content(await pp.RequestAsync(
-                order.ChargedAmount.Value,
-                orderItems,
-                skipReceipt: true,
-                vortoLanguage: order.StoreInfo.Alias,
-                language: "IS", //TODO needs to come from Store, but we can not use culture
-                member: Umbraco.MembershipHelper.GetCurrentMemberId(),
-                orderCustomString: order.UniqueId.ToString(),
-                paymentProviderId: paymentRequest.PaymentProvider.ToString()
-            ));
         }
     }
 
