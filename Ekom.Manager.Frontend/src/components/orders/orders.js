c10cf0b08139511c3d48d57011ffa074384b2723
Ólafur Hólm <olafurholm17@gmail.com>
2018-07-02 10:08:08 +0000

    Ekom Manager front end

--- Ekom.Manager.Frontend/src/components/orders/orders.js
+++ Ekom.Manager.Frontend/src/components/orders/orders.js
@@ -229,27 +229,28 @@ export default class Orders extends Component {
       orderId: UniqueId,
       orderStatus: event.target.value,
     }
-    const xhr = new XMLHttpRequest()
-    xhr.open("post", "/umbraco/backoffice/ekom/managerapi/updatestatus")
-    xhr.onload = () => {
-      const resp = JSON.parse(xhr.response)
-      if (xhr.status >= 200 && xhr.status < 300) {
-        if (resp.success) {
-          //
-          console.log("resp")
-        } else {
-          //
-        }
-      } else {
+    // const xhr = new XMLHttpRequest()
+    // xhr.open("post", "/umbraco/backoffice/ekom/managerapi/updatestatus")
+    // xhr.onload = () => {
+    //   const resp = JSON.parse(xhr.response)
+    //   if (xhr.status >= 200 && xhr.status < 300) {
+    //     if (resp.success) {
+    //       //
+    //       console.log("resp")
+    //     } else {
+    //       //
+    //     }
+    //   } else {
         
-      }
-    }
-    xhr.send(data);
-    /*
+    //   }
+    // }
+    // xhr.send(data);
     orderService().updateStatus(data).then((res) => {
       console.log(res);
     })
-    */
+    .catch(err => {
+      console.log("error: " + err)
+    })
   }
   render() {
     console.log(this.state)
