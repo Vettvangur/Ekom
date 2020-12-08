83de747319104d1cc64c33ed30bbdc29ceb607ea
Gunnar Már Óttarsson <gunni@starcraft.is>
2020-12-08 16:08:29 +0000

    Correctly cast payment providers in api, remove old appveyor file

--- Ekom/API/Providers.cs
+++ Ekom/API/Providers.cs
@@ -81,7 +81,7 @@ namespace Ekom.API
                 store,
                 countryCode,
                 orderAmount
-            ).Cast<ShippingProvider>();
+            ).Cast<IShippingProvider>();
         }
 
 
@@ -107,7 +107,7 @@ namespace Ekom.API
                 store,
                 countryCode,
                 orderAmount
-            ).Cast<PaymentProvider>();
+            ).Cast<IPaymentProvider>();
         }
 
         /// <summary>
