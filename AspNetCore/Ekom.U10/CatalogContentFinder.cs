630631f5fcfe70b406c38802356bd26f3d40b912
Garðar Þorsteinsson <gardar@WellAdvised.is>
2024-09-05 18:24:13 +0000

    try to get node first for content finder

--- AspNetCore/Ekom.U10/CatalogContentFinder.cs
+++ AspNetCore/Ekom.U10/CatalogContentFinder.cs
@@ -9,133 +9,140 @@ using Umbraco.Cms.Core.Routing;
 using Umbraco.Cms.Core.Web;
 using Umbraco.Extensions;
 
-namespace Ekom.Umb
+namespace Ekom.Umb;
+
+class CatalogContentFinder : IContentFinder
 {
-    class CatalogContentFinder : IContentFinder
+    readonly ILogger<CatalogContentFinder> _logger;
+    readonly Configuration _config;
+    readonly IStoreService _storeSvc;
+    readonly IPerStoreCache<ICategory> _categoryCache;
+    readonly IPerStoreCache<IProduct> _productCache;
+    readonly AppCaches _appCaches;
+    readonly IHttpContextAccessor _httpContextAccessor;
+    readonly IUmbracoContextAccessor _umbracoContextAccessor;
+
+    public CatalogContentFinder(
+        ILogger<CatalogContentFinder> logger,
+        Configuration config,
+        IStoreService storeSvc,
+        IPerStoreCache<ICategory> categoryCache,
+        IPerStoreCache<IProduct> productCache,
+        AppCaches appCaches,
+        IHttpContextAccessor httpContextAccessor,
+        IUmbracoContextAccessor umbracoContextAccessor)
     {
-        readonly ILogger<CatalogContentFinder> _logger;
-        readonly Configuration _config;
-        readonly IStoreService _storeSvc;
-        readonly IPerStoreCache<ICategory> _categoryCache;
-        readonly IPerStoreCache<IProduct> _productCache;
-        readonly AppCaches _appCaches;
-        readonly IHttpContextAccessor _httpContextAccessor;
-        readonly IUmbracoContextAccessor _umbracoContextAccessor;
+        _logger = logger;
+        _config = config;
+        _storeSvc = storeSvc;
+        _categoryCache = categoryCache;
+        _productCache = productCache;
+        _appCaches = appCaches;
+        _httpContextAccessor = httpContextAccessor;
+        _umbracoContextAccessor = umbracoContextAccessor;
+    }
 
-        public CatalogContentFinder(
-            ILogger<CatalogContentFinder> logger,
-            Configuration config,
-            IStoreService storeSvc,
-            IPerStoreCache<ICategory> categoryCache,
-            IPerStoreCache<IProduct> productCache,
-            AppCaches appCaches,
-            IHttpContextAccessor httpContextAccessor,
-            IUmbracoContextAccessor umbracoContextAccessor)
+    /// <summary>
+    /// Maps virtual URLs to IPublishedContent items
+    /// Performs various request related processing
+    /// F.x. determining the Store/Currency first from Cookie, then domain and then default
+    /// </summary>
+    public Task<bool> TryFindContent(IPublishedRequestBuilder contentRequest)
+    {
+        try
         {
-            _logger = logger;
-            _config = config;
-            _storeSvc = storeSvc;
-            _categoryCache = categoryCache;
-            _productCache = productCache;
-            _appCaches = appCaches;
-            _httpContextAccessor = httpContextAccessor;
-            _umbracoContextAccessor = umbracoContextAccessor;
-        }
+            var path = contentRequest.Uri.GetAbsolutePathDecoded().ToLower().AddTrailing();
 
-        /// <summary>
-        /// Maps virtual URLs to IPublishedContent items
-        /// Performs various request related processing
-        /// F.x. determining the Store/Currency first from Cookie, then domain and then default
-        /// </summary>
-        public Task<bool> TryFindContent(IPublishedRequestBuilder contentRequest)
-        {
-            try
+            if (path.StartsWith("/umbraco"))
             {
-                var path = contentRequest.Uri.GetAbsolutePathDecoded().ToLower().AddTrailing();
+                return Task.FromResult(false); // Not found
+            }
 
-                if (path.StartsWith("/umbraco"))
+            if (!_umbracoContextAccessor.TryGetUmbracoContext(out var umbracoContext))
+            {
+                return Task.FromResult(false);
+            }
+
+
+            var node = umbracoContext.Content.GetByRoute(path);
+
+            if (node != null && !node.IsDocumentType("ekmProduct") && !node.IsDocumentType("ekmCategory"))
+            {
+                return Task.FromResult(false);
+            }
+
+            var store = _storeSvc.GetStoreByDomain(contentRequest.Domain?.Name, contentRequest.Culture);
+
+            #region Product and/or Category
+
+            // Requesting Product?
+            var product = _productCache.Cache[store.Alias]
+                            .FirstOrDefault(x => x.Value.Urls != null &&
+                                                x.Value.Urls.Contains(path)).Value;
+
+            var contentId = 0;
+            ICategory category;
+
+            if (product != null && !string.IsNullOrEmpty(product.GetValue("slug")))
+            {
+                contentId = product.Id;
+
+                var urlArray = path.Split('/');
+                var categoryUrlArray = urlArray.Take(urlArray.Length - 2);
+                var categoryUrl = string.Join("/", categoryUrlArray).AddTrailing();
+
+                category = _categoryCache.Cache[store.Alias]
+                                        .FirstOrDefault(x => x.Value.Urls.Contains(categoryUrl))
+                                        .Value;
+            }
+            else // Request Category?
+            {
+                category = _categoryCache.Cache[store.Alias]
+                                        .FirstOrDefault(x => x.Value.Urls != null &&
+                                                             x.Value.Urls.Contains(path))
+                                        .Value;
+
+                if (category != null && !string.IsNullOrEmpty(category.GetValue("slug")))
                 {
-                    return Task.FromResult(false); // Not found
-                }
 
-                if (!_umbracoContextAccessor.TryGetUmbracoContext(out var umbracoContext))
+                    contentId = category.Id;
+                }
+                // else Requesting Neither
+            }
+            #endregion
+
+            var httpCtx = _httpContextAccessor.HttpContext;
+            var requestCache = _appCaches.RequestCache.Get("ekmRequest", () => new ContentRequest());
+            if (requestCache != null && requestCache is ContentRequest ekmRequest)
+            {
+                ekmRequest.Store = store;
+                ekmRequest.Product = product;
+                ekmRequest.Category = category;
+
+                ekmRequest.SetStoreCookie(store.Alias, httpCtx);
+            }
+
+            // Request for Product or Category
+            if (contentId != 0)
+            {
+                var content = umbracoContext.Content.GetById(contentId);
+
+                if (content != null && !content.Value<bool>("ekmVirtualUrl"))
+                {
+                    contentRequest.SetPublishedContent(content);
+                    return Task.FromResult(true);
+                }
+                else
                 {
                     return Task.FromResult(false);
                 }
-
-                var store = _storeSvc.GetStoreByDomain(contentRequest.Domain?.Name, contentRequest.Culture);
-
-                #region Product and/or Category
-
-                // Requesting Product?
-                var product = _productCache.Cache[store.Alias]
-                                .FirstOrDefault(x => x.Value.Urls != null &&
-                                                    x.Value.Urls.Contains(path)).Value;
-
-                var contentId = 0;
-                ICategory category;
-
-                if (product != null && !string.IsNullOrEmpty(product.GetValue("slug")))
-                {
-                    contentId = product.Id;
-
-                    var urlArray = path.Split('/');
-                    var categoryUrlArray = urlArray.Take(urlArray.Length - 2);
-                    var categoryUrl = string.Join("/", categoryUrlArray).AddTrailing();
-
-                    category = _categoryCache.Cache[store.Alias]
-                                            .FirstOrDefault(x => x.Value.Urls.Contains(categoryUrl))
-                                            .Value;
-                }
-                else // Request Category?
-                {
-                    category = _categoryCache.Cache[store.Alias]
-                                            .FirstOrDefault(x => x.Value.Urls != null &&
-                                                                 x.Value.Urls.Contains(path))
-                                            .Value;
-
-                    if (category != null && !string.IsNullOrEmpty(category.GetValue("slug")))
-                    {
-
-                        contentId = category.Id;
-                    }
-                    // else Requesting Neither
-                }
-                #endregion
-
-                var httpCtx = _httpContextAccessor.HttpContext;
-                var requestCache = _appCaches.RequestCache.Get("ekmRequest", () => new ContentRequest());
-                if (requestCache != null && requestCache is ContentRequest ekmRequest)
-                {
-                    ekmRequest.Store = store;
-                    ekmRequest.Product = product;
-                    ekmRequest.Category = category;
-
-                    ekmRequest.SetStoreCookie(store.Alias, httpCtx);
-                }
-
-                // Request for Product or Category
-                if (contentId != 0)
-                {
-                    var content = umbracoContext.Content.GetById(contentId);
-
-                    if (content != null && !content.Value<bool>("ekmVirtualUrl"))
-                    {
-                        contentRequest.SetPublishedContent(content);
-                        return Task.FromResult(true);
-                    }
-                    else
-                    {
-                        return Task.FromResult(false);
-                    }
-                }
             }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "Failed to find Ekom content.");
-            }
-
-            return Task.FromResult(false);
         }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Failed to find Ekom content.");
+        }
+
+        return Task.FromResult(false);
     }
 }
