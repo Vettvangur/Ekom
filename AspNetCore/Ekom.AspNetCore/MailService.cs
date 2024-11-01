582cdebf634003699b2cd8a9a6b1b52b4b2c0241
Garðar Þorsteinsson <gardar@WellAdvised.is>
2024-11-01 16:55:46 +0000

    mail error handling

--- AspNetCore/Ekom.AspNetCore/MailService.cs
+++ AspNetCore/Ekom.AspNetCore/MailService.cs
@@ -3,84 +3,89 @@ using Microsoft.Extensions.Configuration;
 using System.Net;
 using System.Net.Mail;
 
-namespace Ekom.AspNetCore.Services
+namespace Ekom.AspNetCore.Services;
+
+/// <summary>
+/// Handles creation and sending of emails, uses defaults from configuration when possible.
+/// Default assumes a notification email intended for the administrator.
+/// All defaults are overridable.
+/// </summary>
+class MailService : IMailService
 {
+    private const int Timeout = 180000;
+    private readonly string _host;
+    private readonly int _port;
+    private readonly string _user;
+    private readonly string _pass;
+
+    private string _sender;
+    private string _recipient;
+
     /// <summary>
-    /// Handles creation and sending of emails, uses defaults from configuration when possible.
-    /// Default assumes a notification email intended for the administrator.
-    /// All defaults are overridable.
+    /// ctor
     /// </summary>
-    class MailService : IMailService
+    public MailService(IConfiguration config)
     {
-        private const int Timeout = 180000;
-        private readonly string _host;
-        private readonly int _port;
-        private readonly string _user;
-        private readonly string _pass;
+        _recipient = Configuration.Instance.EmailNotifications;
 
-        private string _sender;
-        private string _recipient;
+        var cmsSection = config.GetSection("Umbraco:CMS");
+        var smtpSection = cmsSection.GetSection("Global:Smtp");
 
-        /// <summary>
-        /// ctor
-        /// </summary>
-        public MailService(IConfiguration config)
+        //MailServer - Represents the SMTP Server
+        _host = smtpSection["Host"];
+        //Port- Represents the port number
+        _port = 587; 
+
+        if (int.TryParse(smtpSection["Port"], out int port))
         {
-            _recipient = Configuration.Instance.EmailNotifications;
-
-            var cmsSection = config.GetSection("Umbraco:CMS");
-            var smtpSection = cmsSection.GetSection("Global:Smtp");
-
-            //MailServer - Represents the SMTP Server
-            _host = smtpSection["Host"];
-            //Port- Represents the port number
-            _port = 587; 
-
-            if (int.TryParse(smtpSection["Port"], out int port))
-            {
-                _port = port;
-            }
-
-            //MailAuthUser and MailAuthPass - Used for Authentication for sending email
-            _user = smtpSection["Username"];
-            _pass = smtpSection["Password"];
-
-            _sender = smtpSection["From"];
+            _port = port;
         }
 
-        /// <summary>
-        /// Send email message
-        /// </summary>
-        public async virtual Task SendAsync(
-            string subject,
-            string body,
-            string recipient = null,
-            string sender = null)
-        {
-            if (string.IsNullOrEmpty(_host))
-            {
-                throw new Exception("Umbraco:CMS:Global:Smtp:Host is not configured");
-            }
-            
-            // We do not catch the error here... let it pass direct to the caller
-            using (var smtp = new SmtpClient(_host, _port))
-            using (var message = new MailMessage(
-                sender ?? _sender,
-                recipient ?? _recipient,
-                subject,
-                body)
-            { IsBodyHtml = true })
-            {
-                if (_user.Length > 0 && _pass.Length > 0)
-                {
-                    smtp.Timeout = Timeout;
-                    smtp.UseDefaultCredentials = false;
-                    smtp.Credentials = new NetworkCredential(_user, _pass);
-                    smtp.EnableSsl = true;
-                }
+        //MailAuthUser and MailAuthPass - Used for Authentication for sending email
+        _user = smtpSection["Username"];
+        _pass = smtpSection["Password"];
 
-                await smtp.SendMailAsync(message).ConfigureAwait(false);
+        _sender = smtpSection["From"];
+    }
+
+    /// <summary>
+    /// Send email message
+    /// </summary>
+    public async virtual Task SendAsync(
+        string subject,
+        string body,
+        string recipient = null,
+        string sender = null)
+    {
+        if (string.IsNullOrEmpty(_host))
+        {
+            throw new Exception("Umbraco:CMS:Global:Smtp:Host is not configured");
+        }
+        
+
+        if (string.IsNullOrEmpty(recipient) && string.IsNullOrEmpty(_recipient))
+        {
+            throw new Exception("Ekom:Ekom:EmailNotifications is not set.");
+        }
+
+        // We do not catch the error here... let it pass direct to the caller
+        using (var smtp = new SmtpClient(_host, _port))
+        using (var message = new MailMessage(
+            sender ?? _sender,
+            recipient ?? _recipient,
+            subject,
+            body)
+        { IsBodyHtml = true })
+        {
+            if (_user.Length > 0 && _pass.Length > 0)
+            {
+                smtp.Timeout = Timeout;
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new NetworkCredential(_user, _pass);
+                smtp.EnableSsl = true;
             }
+
+            await smtp.SendMailAsync(message).ConfigureAwait(false);
         }
     }
 }
