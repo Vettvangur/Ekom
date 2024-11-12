8e9e204e6053819761964361742dde75acb507b4
1ngimar <ingimarlogig@gmail.com>
2024-11-12 13:34:34 +0000

    add critical log message when fullsync fails

--- AspNetCore/Ekom.U10/Services/ImportService.cs
+++ AspNetCore/Ekom.U10/Services/ImportService.cs
@@ -138,6 +138,11 @@ public class ImportService : IImportService
                 "Full Sync took {Duration} seconds. Categories Saved: {categoriesCount} Products Saved: {productsCount} Variants Saved: {variantsCount} VariantsGroups Saved: {variantGroupsCount} Categories Deleted: {categoriesDeleted} Products Deleted: {productDeleted} Variants Deleted: {variantDeleted} VariantsGroups Deleted: {variantGroupDeleted}", 
                 (stopwatch.ElapsedMilliseconds / 1000.0).ToString("F2"), categoriesSaved.Count, productsSaved.Count, variantsSaved.Count, variantGroupsSaved.Count, categoriesDeleted, productDeleted, variantDeleted, variantGroupDeleted);
         }
+        catch (Exception ex)
+        {
+            _logger.LogCritical(ex, "Full Sync failed");
+            throw;
+        }
         finally
         {
             lock (_syncLock)
