19852f21dc0ae394654567e78102dfd4c132fa40
1ngimar <ingimarlogig@gmail.com>
2024-11-12 13:35:15 +0000

    revert the ekmCategoryRespository check

--- AspNetCore/Ekom.U10/Services/ImportService.cs
+++ AspNetCore/Ekom.U10/Services/ImportService.cs
@@ -105,7 +105,7 @@ public class ImportService : IImportService
 
             ArgumentNullException.ThrowIfNull(umbracoRootContent);
             var allUmbracoCategories = allEkomNodes
-                .Where(x => (x.ContentType.Alias == "ekmCategory" || x.ContentType.Alias == "ekmCategoryRepository") && x.Path.Contains(umbracoRootContent.Id.ToString(), StringComparison.InvariantCulture))
+                .Where(x => x.ContentType.Alias == "ekmCategory" && x.Path.Contains(umbracoRootContent.Id.ToString(), StringComparison.InvariantCulture))
                 .Where(x => x.Path.Split(',').Contains(umbracoRootContent.Id.ToString())).ToList();
             var allUmbracoProducts = allEkomNodes.Where(x => x.ContentType.Alias == "ekmProduct")
                 .ToList();
