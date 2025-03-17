9c81e573f8dc00727946a20a7c7bdd8e9d60a49e
Garðar Þorsteinsson <gardar@vettvangur.is>
2025-03-17 15:10:20 +0000

    default values on search request

--- Ekom/Models/ProductQueryBase.cs
+++ Ekom/Models/ProductQueryBase.cs
@@ -1,14 +1,13 @@
-namespace Ekom.Models
+namespace Ekom.Models;
+
+public class ProductQueryBase
 {
-    public class ProductQueryBase
-    {
-        public int? Page { get; set; }
-        public int? PageSize { get; set; }
-        public string SearchQuery { get; set; }
-        public IEnumerable<int> Ids { get; set; }
-        public IEnumerable<Guid> Keys { get; set; }
-        public List<EkomSearchField> SearchFields { get; set; } = null;
-        public string StoreAlias { get; set; } = null;
-        public bool AllFiltersVisible { get; set; } = false;
-    }
+    public int? Page { get; set; } 
+    public int? PageSize { get; set; } 
+    public string SearchQuery { get; set; } = string.Empty;
+    public IEnumerable<int> Ids { get; set; } = new List<int>();
+    public IEnumerable<Guid> Keys { get; set; } = new List<Guid>();
+    public List<EkomSearchField> SearchFields { get; set; } = new();
+    public string StoreAlias { get; set; } = string.Empty;
+    public bool AllFiltersVisible { get; set; } = false;
 }
--- Ekom/Models/SearchRequest.cs
+++ Ekom/Models/SearchRequest.cs
@@ -1,30 +1,29 @@
 using Ekom.Utilities;
 
-namespace Ekom.Models
-{
-    public class SearchRequest : ProductQueryBase
-    {
-        public string SearchNodeById { get; set; } = "";
-        public string[] NodeTypeAlias { get; set; } = null;
-        public Dictionary<string, List<string>> MetaFilters { get; set; }
-        public Dictionary<string, List<string>> PropertyFilters { get; set; }
-        public OrderBy OrderBy { get; set; } = OrderBy.NoOrder;
-        public string ExamineIndex { get; set; }
-        public string StoreAlias { get; set; }
-    }
+namespace Ekom.Models;
 
-    public class EkomSearchField
-    {
-        public string Name { get; set; }
-        public EkomSearchType SearchType { get; set; } = EkomSearchType.FuzzyAndWilcard;
-        public string FuzzyConfiguration { get; set; } = "0.5";
-        public string Booster { get; set; }
-    }
-    public enum EkomSearchType
-    {
-        Exact,
-        Wildcard,
-        Fuzzy,
-        FuzzyAndWilcard
-    }
+public class SearchRequest : ProductQueryBase
+{
+    public string SearchNodeById { get; set; } = "";
+    public string[] NodeTypeAlias { get; set; } = Array.Empty<string>();
+    public Dictionary<string, List<string>> MetaFilters { get; set; } = new();
+    public Dictionary<string, List<string>> PropertyFilters { get; set; } = new();
+    public OrderBy OrderBy { get; set; } = OrderBy.NoOrder;
+    public string ExamineIndex { get; set; } = string.Empty;
+    public string StoreAlias { get; set; } = string.Empty;
+}
+
+public class EkomSearchField
+{
+    public string Name { get; set; }
+    public EkomSearchType SearchType { get; set; } = EkomSearchType.FuzzyAndWilcard;
+    public string FuzzyConfiguration { get; set; } = "0.5";
+    public string Booster { get; set; }
+}
+public enum EkomSearchType
+{
+    Exact,
+    Wildcard,
+    Fuzzy,
+    FuzzyAndWilcard
 }
