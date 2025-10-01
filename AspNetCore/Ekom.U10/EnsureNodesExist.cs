15038bdb28b97dfaf3327d7979446b9cac69cc58
Garðar Þorsteinsson <gardar@vettvangur.is>
2025-10-01 22:39:28 +0000

    fix: variant sync failed

--- AspNetCore/Ekom.U10/EnsureNodesExist.cs
+++ AspNetCore/Ekom.U10/EnsureNodesExist.cs
@@ -1110,6 +1110,7 @@ class EnsureNodesExist : IComponent
                                 new PropertyType(_shortStringHelper, booleanDt, "shareBasketBetweenStores")
                                 {
                                     Name = "Share Basket Between Stores",
+                                    Description = "This will allow baskets to be shared between stores but we aware that it requires the same currencies to be available cross stores."
                                 },                                
                                 new PropertyType(_shortStringHelper, booleanDt, "applyVatOnShipping")
                                 {
