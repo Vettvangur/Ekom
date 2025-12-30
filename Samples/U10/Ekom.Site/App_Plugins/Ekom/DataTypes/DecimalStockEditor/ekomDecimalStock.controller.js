aa72ea9bc0390f02ddcfdf44cc8acd8bd9601753
1ngimar <ingimarlogig@gmail.com>
2025-12-30 15:56:44 +0000

    Add custom Decimal Stock Editor for Umbraco
    Introduced a new `EkomDecimalStockEditor` class to define a custom data editor for managing decimal stock values in Umbraco. The editor uses a JSON value type and includes a JavaScript asset for functionality.
    
    Added an AngularJS controller `Ekom.DecimalStock` to handle stock data logic, including initialization, fetching stock values for single and multi-store setups, and updating the model on form submission.
    
    Created an HTML template `ekomDecimalStock.html` to provide a UI for managing stock values, supporting dynamic rendering of input fields for multiple stores.

---
+++ Samples/U10/Ekom.Site/App_Plugins/Ekom/DataTypes/DecimalStockEditor/ekomDecimalStock.controller.js
@@ -0,0 +1,100 @@
+angular.module('umbraco').controller('Ekom.DecimalStock', function ($scope, $routeParams, $http, contentResource, $q) {
+
+  if ($routeParams.section !== 'content') { return; }
+
+  $scope.stocks = [];
+  $scope.content = null;
+  $scope.config = null;
+  $scope.perStoreStock = false;
+  $scope.model.hideLabel = false;
+
+  $scope.getStockData = function () {
+
+    if ($scope.perStoreStock) {
+
+
+      $http.get(Umbraco.Sys.ServerVariables.ekom.apiEndpoint + 'Stores').then(function (results) {
+
+        if (results.data.length > 1) {
+          results.data.forEach(function (store) {
+
+            getStockValue(store.alias).then(function (stockValue) {
+              $scope.stocks.push({
+                storeAlias: store.alias,
+                value: stockValue
+              });
+            })
+          });
+        } else {
+          getStockValue('').then(function (stockValue) {
+            $scope.stocks.push({
+              storeAlias: '',
+              value: stockValue
+            });
+          })
+        }
+      });
+
+    } else {
+
+      getStockValue('').then(function (stockValue) {
+        $scope.stocks.push({
+          storeAlias: '',
+          value: stockValue
+        });
+      })
+    }
+
+  };
+
+  var getStockValue = function (storeAlias) {
+
+    if ($scope.content.contentTypeAlias !== 'ekmProduct' && $scope.content.contentTypeAlias !== 'ekmProductVariant') {
+      return $q.when(0);
+    }
+
+    if (storeAlias !== '') {
+      return $http.get(Umbraco.Sys.ServerVariables.ekom.backofficeApiEndpoint + 'Stock/' + $scope.content.key + "/storeAlias/" + storeAlias)
+        .then(function (result) {
+          return parseFloat(result.data);
+        });
+    } else {
+      return $http.get(Umbraco.Sys.ServerVariables.ekom.backofficeApiEndpoint + 'Stock/' + $scope.content.key)
+        .then(function (result) {
+          return parseFloat(result.data);
+        });
+    }
+  }
+
+  $scope.init = function () {
+    if ($routeParams.section == 'content') {
+
+      contentResource.getById($routeParams.id)
+        .then(function (data) {
+          $scope.content = data;
+
+          $http.get(Umbraco.Sys.ServerVariables.ekom.backofficeApiEndpoint + 'Config').then(function (result) {
+
+            $scope.config = result.data;
+
+            if ($scope.config.perStoreStock) {
+
+              $scope.perStoreStock = true;
+            }
+
+            $scope.getStockData();
+
+          });
+
+        });
+    }
+
+  };
+
+  $scope.init();
+
+  $scope.$on("formSubmitting", function () {
+    $scope.model.value = $scope.stocks;
+  });
+
+});
