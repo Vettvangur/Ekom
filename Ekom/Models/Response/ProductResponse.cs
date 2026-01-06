2591fdf17754a24ee033ee2a5b0013ac0f2fb00b
Garðar Þorsteinsson <gardar@vettvangur.is>
2026-01-06 09:09:42 +0000

    fix: filter out zero prices listens to variant prices to

--- Ekom/Models/Response/ProductResponse.cs
+++ Ekom/Models/Response/ProductResponse.cs
@@ -21,7 +21,7 @@ public class ProductResponse
             // Filter out zero-price products
             if (query.FilterOutZeroPriceProducts)
             {
-                products = products.Where(x => x.OriginalPrice?.Value > 0);
+                products = products.Where(x =>  x.PrimaryVariant != null  ? x.PrimaryVariant.OriginalPrice?.Value > 0 : x.OriginalPrice?.Value > 0);
             }
 
             // Store the total number of products before any filtering
