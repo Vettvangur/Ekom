73e6bdfbc1ace399df3615e89ffd2b10fdc9563c
Garðar Þorsteinsson <gardar@vettvangur.is>
2026-01-29 13:59:36 +0000

    fix: klaviyo time format, remove retry in cache

--- Plugins/Ekom.Klaviyo/Mappers/OrderMapper.cs
+++ Plugins/Ekom.Klaviyo/Mappers/OrderMapper.cs
@@ -155,7 +155,7 @@ public static class OrderMapper
                     }
                 },
 
-                time = o.PlacedAt,
+                time = o.PlacedAt.ToUniversalTime().ToString("yyyy-MM-dd'T'HH:mm:ss.fff'Z'"),
                 properties
             }
         };
